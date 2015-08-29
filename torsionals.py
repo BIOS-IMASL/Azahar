e237f06d059a19cc54c490828ed6ce976ff4aab4
aloctavodia <aloctavodia@gmail.com>
2015-08-28 23:50:37 -0300

    fix typo that prevents the torsionals to be properly readed

--- torsionals.py
+++ torsionals.py
@@ -6,14 +6,15 @@ from pymol import cmd
 import numpy as np
 
 
-def get_phi(obj, bond, state=0):
+def get_phi(obj, bond, state=1):
     """
     Get the dihedral angle phi (OR-C1-O'x-C'x) 
     """
     resi_i, resi_j, atom_i, atom_j = bond[0], bond[2], bond[4], bond[5]
+
     if atom_i > atom_j:
         atom_j = atom_i
-        res_i, resi_j = resi_j, resi_i
+        resi_i, resi_j = resi_j, resi_i
     atom1 = '%s and resi %s and name OR' % (obj, resi_i)
     atom2 = '%s and resi %s and name C1' % (obj, resi_i)
     atom3 = '%s and resi %s and name O%s' % (obj, resi_j, atom_j)
@@ -22,14 +23,14 @@ def get_phi(obj, bond, state=0):
     return value
 
 
-def get_psi(obj, bond, state=0): 
+def get_psi(obj, bond, state=1): 
     """
     Get the dihedral angle psi (C1-O'x-C'x-C'x-1)
     """
     resi_i, resi_j, atom_i, atom_j = bond[0], bond[2], bond[4], bond[5]
     if atom_i > atom_j:
         atom_j = atom_i
-        res_i, resi_j = resi_j, resi_i
+        resi_i, resi_j = resi_j, resi_i
     atom1 = '%s and resi %s and name C1' % (obj, resi_i)
     atom2 = '%s and resi %s and name O%s' % (obj, resi_j, atom_j)
     atom3 = '%s and resi %s and name C%s' % (obj, resi_j, atom_j)
@@ -38,7 +39,7 @@ def get_psi(obj, bond, state=0):
     return value  
 
 
-def get_omega(obj, bond, state=0): 
+def get_omega(obj, bond, state=1): 
     """
     Get the dihedral angle omega (O6-C6-C5-C4)
     """
@@ -55,14 +56,14 @@ def get_chi():
     pass
 
 
-def set_phi(obj, bond, angle, state=0):
+def set_phi(obj, bond, angle, state=1):
     """
     Set the dihedral angle phi (OR-C1-O'x-C'x) 
     """
     resi_i, resi_j, atom_i, atom_j = bond[0], bond[2], bond[4], bond[5]
     if atom_i > atom_j:
         atom_j = atom_i
-        res_i, resi_j = resi_j, resi_i
+        resi_i, resi_j = resi_j, resi_i
     atom1 = '%s and resi %s and name OR' % (obj, resi_i)
     atom2 = '%s and resi %s and name C1' % (obj, resi_i)
     atom3 = '%s and resi %s and name O%s' % (obj, resi_j, atom_j)
@@ -71,14 +72,14 @@ def set_phi(obj, bond, angle, state=0):
 
 
 
-def set_psi(obj, bond, angle, state=0): 
+def set_psi(obj, bond, angle, state=1): 
     """
     Set the dihedral angle psi (C1-O'x-C'x-C'x-1)
     """
     resi_i, resi_j, atom_i, atom_j = bond[0], bond[2], bond[4], bond[5]
     if atom_i > atom_j:
         atom_j = atom_i
-        res_i, resi_j = resi_j, resi_i
+        resi_i, resi_j = resi_j, resi_i
     atom1 = '%s and resi %s and name C1' % (obj, resi_i)
     atom2 = '%s and resi %s and name O%s' % (obj, resi_j, atom_j)
     atom3 = '%s and resi %s and name C%s' % (obj, resi_j, atom_j)
@@ -86,7 +87,7 @@ def set_psi(obj, bond, angle, state=0):
     cmd.set_dihedral(atom1, atom2, atom3, atom4, angle, state=state)
   
 
-def set_omega(obj, bond, angle, state=0): 
+def set_omega(obj, bond, angle, state=1): 
     """
     Get the dihedral angle omega (O6-C6-C5-C4)
     """
@@ -98,7 +99,7 @@ def set_omega(obj, bond, angle, state=0):
     cmd.set_dihedral(atom1, atom2, atom3, atom4, angle, state=state)
 
                  
-def set_chi(obj, bond, state=0):
+def set_chi(obj, bond, state=1):
     """
     Set the dihedral angles chi (Cx-1-Cx-O'x-H'x) to one of tree predefinied 
     angles (-60, 60, 180). The perturbed chi angle is selected at random.
